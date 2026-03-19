f37859adcbd93a169fadbd7279d49077fef89894
Ростислав Дурягін <rostyslav.duriahin@lnu.edu.ua>
2026-03-19 19:39:02 +0200

    Complete

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
+from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
-from typing import Any
 
 
-def go_to_cafe(friends: list[dict], cafe: Any) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
 
     for friend in friends:
